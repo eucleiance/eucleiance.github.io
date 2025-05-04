995cf9775ee9ef4f2f2c46bdfa3806ce252d7c69
eucleiance <eucleiance@gmail.com>
2025-05-04 22:09:07 +0545

    background fix?

--- src/Components/Landing.tsx
+++ src/Components/Landing.tsx
@@ -3,7 +3,7 @@ const Landing = () => {
     <div className="flex justify-center items-center min-h-screen">
       <div className="text-center font-light text-2xl">
         <a href="#Projects" className="cursor-pointer">
-          Hello
+          Hello Stranger
         </a>
       </div>
     </div>
