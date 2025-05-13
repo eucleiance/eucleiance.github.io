19427726e3e3ad5c0f08371269b8dc129b6f5bcc
eucleiance <eucleiance@gmail.com>
2025-05-13 23:23:19 +0545

    projects & about & footer

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,24 +1,27 @@
-import Scramble from "./Scramble"
+// import Scramble from "./Scramble"
 import { useScramble } from "../Hooks/useScramble"
 
 const About = () => {
-  const scrambleArray = [
-    "Über mich",
-    "About me",
-  ]
+  // const scrambleArray = [
+  //   "Über mich",
+  //   "About me",
+  // ]
 
-  useScramble(0.2);
+  useScramble(0.1);
 
 
   return (
     <div id="about" className="flex justify-center items-center min-h-screen bg-rose-50">
-      <div className=" text-gray-950 text-xl font-xanh-regular mx-7">
-        <Scramble
+      <div className=" text-gray-950 text-xl md:text-4xl font-canela-thin mx-7 text-center leading-14">
+        <div scramble-to="Hallo!">Hello! </div>
+        <div scramble-to="Ich bin Rosh">I'm Rosh</div>
+
+        {/* <Scramble
           scramArr={scrambleArray}
           duration={2}
           revealDelay={0.2}
           className={"text-gray-950 customcursor"}
-        />
+        /> */}
         {/* <div scramble-to="Haus" className="customcursor text-center">Home</div>
         <div scramble-to="Projekts" className="customcursor text-center">Projects</div> */}
       </div>
--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -1,28 +1,44 @@
-import Scramble from "./Scramble"
+// import Scramble from "./Scramble"
 import { useScramble } from "../Hooks/useScramble"
 
 const Projects = () => {
-  const scrambleArray = [
-    "Project A",
-    "Project B",
-    "Project C",
+  const projects = [
+    { name: "ICT Everything", deets: "TradingView Indicator", url: "https://www.tradingview.com/script/T6KkMfK6-ICT-Everything/", scrambleText: "Automated Forex & Futures Chart Elements" },
+    { name: "Huesmith", deets: "VSCode Extension", url: "https://open-vsx.org/extension/Eucleiance/huesmith" , scrambleText: "One-Click Theme Override for VSCod(e/ium)"},
+    { name: "Average Range", deets: "TradingView Indicator", url: "https://www.tradingview.com/script/d1bNjbFy-Average-Range-coldbrewrosh/", scrambleText: "Automated Range Calculator for Forex" },
+    { name: "ChronoNote", deets: "Note App", url: "https://chrononote.vercel.app/" , scrambleText: "Note App with Timeline Playback"},
   ]
 
-  useScramble(0.2);
-
+  useScramble(0.1)
 
   return (
     <div id="projects" className="flex justify-center items-center min-h-screen bg-gray-950">
-      <div className=" text-rose-50 text-xl font-xanh-regular mx-7">
-        <Scramble
+      <ul className="text-rose-50 text-lg md:text-4xl lg:text-6xl font-canela-thin w-full max-w-7xl px-7">
+        {projects.map((project, index) => (
+          <div
+            key={index}
+            className="border-b border-rose-50 pointer-events-none py-3 md:py-6"
+          >
+            <a
+              href={project.url}
+              className="flex justify-between pointer-events-auto"
+              target="_blank" rel="noopener noreferrer"
+            >
+              <span scramble-to={project.scrambleText} className="w-full">{project.name}</span>
+              {/* <span className="text-xs md:text-lg self-end tracking-wider">{project.deets}</span> */}
+            </a>
+          </div>
+        ))}
+      </ul>
+
+      {/* <Scramble
           scramArr={scrambleArray}
           duration={1}
           revealDelay={0.1}
-          className={"text-rose-50 customcursor"}
-        />
-        {/* <div scramble-to="Haus" className="customcursor text-center">Home</div>
+          className="text-rose-50 customcursor"
+        /> */}
+      {/* <div scramble-to="Haus" className="customcursor text-center">Home</div>
         <div scramble-to="Projekts" className="customcursor text-center">Projects</div> */}
-      </div>
     </div>
   )
 }
